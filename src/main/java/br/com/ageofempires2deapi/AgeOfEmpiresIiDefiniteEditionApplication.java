ff4ff087ba649101fba37e5a9ae1983ae7ad2cb3
Gabriel Freire <gabrbfreire@gmail.com>
2020-12-19 19:34:34 -0300

    first commit

---
+++ src/main/java/br/com/ageofempires2deapi/AgeOfEmpiresIiDefiniteEditionApplication.java
@@ -0,0 +1,13 @@
+package br.com.ageofempires2deapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgeOfEmpiresIiDefiniteEditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgeOfEmpiresIiDefiniteEditionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ageofempires2deapi/AgeOfEmpiresIiDefiniteEditionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ageofempires2deapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgeOfEmpiresIiDefiniteEditionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
