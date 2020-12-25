4d7b492d9fbad80e2499ae56e93126bffeb00d22
Gabriel Freire <gabrbfreire@gmail.com>
2020-12-25 11:51:17 -0300

    Buildings findAllByNameContaining

--- src/main/java/br/com/age2deapi/controller/BuildingsController.java
+++ src/main/java/br/com/age2deapi/controller/BuildingsController.java
@@ -7,10 +7,13 @@ import br.com.age2deapi.model.Civilizations;
 import br.com.age2deapi.repository.BuildingsRepository;
 import br.com.age2deapi.repository.CivilizationsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BuildingsController {
@@ -21,6 +24,12 @@ public class BuildingsController {
     @GetMapping("/buildings")
     public List<BuildingsDto> getAllBuildings(){
         List<Buildings> buildingsList = buildingsRepository.findAll();
+        return BuildingsDto.convertToDto(buildingsList);
+    }
+
+    @GetMapping("/buildings/{name}")
+    public List<BuildingsDto> getBuildingById(@PathVariable String name){
+        List<Buildings> buildingsList = buildingsRepository.findAllByNameContaining(name);
         return BuildingsDto.convertToDto(buildingsList);
     }
 }
--- src/main/java/br/com/age2deapi/repository/BuildingsRepository.java
+++ src/main/java/br/com/age2deapi/repository/BuildingsRepository.java
@@ -3,5 +3,10 @@ package br.com.age2deapi.repository;
 import br.com.age2deapi.model.Buildings;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BuildingsRepository extends JpaRepository<Buildings, Long> {
+
+    List<Buildings> findAllByNameContaining(String name);
 }
