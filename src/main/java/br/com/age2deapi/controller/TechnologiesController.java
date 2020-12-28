7b52b5b8b992f8c59e8264250168c46ae3bbebd6
Gabriel Freire <gabrbfreire@gmail.com>
2020-12-28 12:54:30 -0300

    getTechnologiesByName

--- src/main/java/br/com/age2deapi/controller/TechnologiesController.java
+++ src/main/java/br/com/age2deapi/controller/TechnologiesController.java
@@ -1,10 +1,13 @@
 package br.com.age2deapi.controller;
 
+import br.com.age2deapi.controller.dto.CivilizationsDto;
 import br.com.age2deapi.controller.dto.TechnologiesDto;
+import br.com.age2deapi.model.Civilizations;
 import br.com.age2deapi.model.Technologies;
 import br.com.age2deapi.repository.TechnologiesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,6 +21,12 @@ public class TechnologiesController {
     @GetMapping("/technologies")
     public List<TechnologiesDto> getAllTechnologies(){
         List<Technologies> technologiesList = technologiesRepository.findAll();
+        return TechnologiesDto.convertToDto(technologiesList);
+    }
+
+    @GetMapping("/technologies/{name}")
+    public List<TechnologiesDto> getTechnologiesByName(@PathVariable String name){
+        List<Technologies> technologiesList = technologiesRepository.findAllByNameContaining(name);
         return TechnologiesDto.convertToDto(technologiesList);
     }
 }
--- src/main/java/br/com/age2deapi/repository/TechnologiesRepository.java
+++ src/main/java/br/com/age2deapi/repository/TechnologiesRepository.java
@@ -3,5 +3,8 @@ package br.com.age2deapi.repository;
 import br.com.age2deapi.model.Technologies;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TechnologiesRepository extends JpaRepository<Technologies, Long> {
+    List<Technologies> findAllByNameContaining(String name);
 }
