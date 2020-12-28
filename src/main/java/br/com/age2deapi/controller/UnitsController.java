37230c015a5cd5b12ddfa32aa732ff9baceb278b
Gabriel Freire <gabrbfreire@gmail.com>
2020-12-28 12:57:39 -0300

    getUnitsByName

--- src/main/java/br/com/age2deapi/controller/UnitsController.java
+++ src/main/java/br/com/age2deapi/controller/UnitsController.java
@@ -1,10 +1,13 @@
 package br.com.age2deapi.controller;
 
+import br.com.age2deapi.controller.dto.TechnologiesDto;
 import br.com.age2deapi.controller.dto.UnitsDto;
+import br.com.age2deapi.model.Technologies;
 import br.com.age2deapi.model.Units;
 import br.com.age2deapi.repository.UnitsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,6 +21,12 @@ public class UnitsController {
     @GetMapping("/units")
     public List<UnitsDto> getAllUnits(){
         List<Units> unitsList = unitsRepository.findAll();
+        return UnitsDto.convertToDto(unitsList);
+    }
+
+    @GetMapping("/units/{name}")
+    public List<UnitsDto> getUnitsByName(@PathVariable String name){
+        List<Units> unitsList = unitsRepository.findAllByNameContaining(name);
         return UnitsDto.convertToDto(unitsList);
     }
 }
--- src/main/java/br/com/age2deapi/repository/UnitsRepository.java
+++ src/main/java/br/com/age2deapi/repository/UnitsRepository.java
@@ -1,7 +1,11 @@
 package br.com.age2deapi.repository;
 
+import br.com.age2deapi.controller.dto.UnitsDto;
 import br.com.age2deapi.model.Units;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UnitsRepository extends JpaRepository<Units, Long> {
+    List<Units> findAllByNameContaining(String name);
 }
