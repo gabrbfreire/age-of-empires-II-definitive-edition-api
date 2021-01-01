5324564064722d3b06e42cd32fac9b370c5be17f
Gabriel Freire <gabrbfreire@gmail.com>
2021-01-01 11:39:10 -0300

    Changed to ResponseEntity return type

--- src/main/java/br/com/age2deapi/controller/BuildingsController.java
+++ src/main/java/br/com/age2deapi/controller/BuildingsController.java
@@ -5,6 +5,7 @@ import br.com.age2deapi.model.Buildings;
 import br.com.age2deapi.repository.BuildingsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +27,11 @@ public class BuildingsController {
 
     @GetMapping("/buildings/{name}")
     @Cacheable(value = "getBuildingsByName")
-    public List<BuildingsDto> getBuildingsByName(@PathVariable String name){
+    public ResponseEntity<List<BuildingsDto>> getBuildingsByName(@PathVariable String name){
         List<Buildings> buildingsList = buildingsRepository.findAllByNameStartingWithOrderByName(name);
-        return BuildingsDto.convertToDto(buildingsList);
+        if(!BuildingsDto.convertToDto(buildingsList).isEmpty()){
+            return ResponseEntity.ok(BuildingsDto.convertToDto(buildingsList));
+        }
+        return ResponseEntity.notFound().build();
     }
 }
